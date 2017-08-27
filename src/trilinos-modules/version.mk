97c625b965b5f61d0f48e7f48931816bd95d6ad8
Jim Hayes <jhayes@sdsc.edu>
2017-08-27 12:19:37 -0700

    Obsolete compiler-specific module rpms.

--- src/trilinos-modules/version.mk
+++ src/trilinos-modules/version.mk
@@ -9,4 +9,4 @@ VERSION_SRC = $(REDHAT.ROOT)/src/$(PACKAGE)/version.mk
 VERSION_INC = version.inc
 include $(VERSION_INC)
 
-RPM.EXTRAS  = AutoReq:No
+RPM.EXTRAS  = AutoReq:No\nObsoletes:sdsc-trilinos_gnu-modules,sdsc-trilinos_intel-modules,sdsc-trilinos_pgi-modules
